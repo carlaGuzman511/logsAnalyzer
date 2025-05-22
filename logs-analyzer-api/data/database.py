ad5b166f6921b43a0b7c00a2cd11adf703df6f40
Carla Guzman Suse15 <mirandagcmarcela@gmail.com>
2025-05-21 20:37:09 -0400

    adding run.sh file

--- logs-analyzer-api/data/database.py
+++ logs-analyzer-api/data/database.py
@@ -1,9 +1,13 @@
 from peewee import MySQLDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db = MySQLDatabase(
-    'logs_db',
-    user='aso',
-    password='aso',
-    host='localhost',
-    port=3306
+    os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=int(os.getenv('DB_PORT'))
 )
