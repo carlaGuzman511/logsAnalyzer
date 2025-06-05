4ff7cf841b15a36e2465d14e130a5864f66d71f7
Carla Guzman Suse15 <mirandagcmarcela@gmail.com>
2025-06-05 15:55:44 -0400

    fix regex on ftp

--- logs-analyzer-api/parsers/vsftpd_log_parser.py
+++ logs-analyzer-api/parsers/vsftpd_log_parser.py
@@ -2,15 +2,15 @@ import re
 from datetime import datetime
 
 vsftpd_regex = re.compile(
-    r'^(?P<timestamp>\w{3}\s+\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}\s+\d{4})\s+'       # Fecha
+    r'^(?P<timestamp>\w{3}\s+\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}\s+\d{4})\s+'
     r'(?:vsftpd\s+)?'
-    r'\[pid\s+(?P<pid>\d+)\]\s*'                                                   # PID
-    r'(?:\[(?P<user>[^\]]+)\])?\s*'                                                # Usuario opcional
-    r'(?:(?P<action>[A-Z ]+):)?\s*'                                                # Acción (FTP command, OK LOGIN, etc.)
-    r'(?:Client\s+"(?P<ip>[\d.]+)")?'                                              # IP opcional (para logs con "Client")
-    r'(?:,\s+"(?P<message>[^"]*)")?'                                               # Mensaje principal entre comillas (opcional)
-    r'(?:,\s+"(?P<extra_message>[^"]*)")?'                                         # Mensaje adicional entre comillas (opcional)
-    r'.*'                                                                          # Captura cualquier resto (para líneas multilinea del tipo "211-Features")
+    r'\[pid\s+(?P<pid>\d+)\]\s*'
+    r'(?:\[(?P<user>[^\]]+)\])?\s*'
+    r'(?:(?P<action>[^:]+):)?\s*'   
+    r'(?:Client\s+"(?P<ip>[\d.]+)")?'
+    r'(?:,\s+"(?P<message>[^"]*)")?'
+    r'(?:,\s+"(?P<extra_message>[^"]*)")?'
+    r'.*'
 )
 
 vsftpd_internal_regex = re.compile(
